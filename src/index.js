81d4b5f7d3cda4aee51c9e66aa568d44f4907bd0
Zack Stickles <zstickles@gmail.com>
2019-02-06 20:17:05 -0600

    Added React

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('this is a test');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Test = () => (
+  <div>
+    This is a test react component
+  </div>
+);
+
+ReactDOM.render(<Test />, document.getElementById('app'));
