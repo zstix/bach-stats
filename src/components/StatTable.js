188eac10a07b43538315255964d9116062d77c33
Zack Stickles <zstickles@gmail.com>
2019-02-06 20:45:50 -0600

    Added a basic tabel

---
+++ src/components/StatTable.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+});
+
+const StatTable = (props) => {
+  const { classes, data } = props;
+
+  return (
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">Age</TableCell>
+            <TableCell align="right">Favorite Color</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map(row => (
+            <TableRow key={row.id}>
+              <TableCell component="th" scope="row">{row.name}</TableCell>
+              <TableCell align="right">{row.age}</TableCell>
+              <TableCell align="right">{row.favColor}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+};
+
+StatTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+  data: PropTypes.array.isRequired,
+};
+
+export default withStyles(styles)(StatTable);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
-const Test = () => (
-  <div>
-    This is a test react component
-  </div>
+import StatTable from './components/StatTable';
+
+// TODO: add in actual data
+const data = [
+  { name: 'Zack', age: 32, favColor: 'green' },
+  { name: 'Nina', age: 29, favColor: 'blue' },
+  { name: 'Alex', age: 26, favColor: 'red' },
+];
+
+const App = () => (
+  <React.Fragment>
+    <CssBaseline />
+    <StatTable data={data} />
+  </React.Fragment>
 );
 
-ReactDOM.render(<Test />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
