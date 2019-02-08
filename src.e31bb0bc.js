21d074f92531079d0f1556c00e9636aa2df81196
Zack Stickles <zstickles@gmail.com>
2019-02-08 05:19:09 -0600

    Rebuild

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -43700,7 +43700,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51243" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55675" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
